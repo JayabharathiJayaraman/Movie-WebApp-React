23669f91f16e8ab63c4e147a3762fa2d052ebb71
David Dahlman <daviddahlman82@gmail.com>
2021-05-23 17:33:00 +0200

    first search version

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,8 +4,11 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actions, STATUS } from "../features/movie";
 
-const Search = ({ placeholder, handleChange }) => {
-    
+const Search = ({ placeholder}) => {
+    const movies = useSelector(state => state.movie.movies);
+    const dispatch = useDispatch()
+    const handleChange = event => setSearchMovie(event.target.value);
+    const [searchMovie, setSearchMovie] = useState('')
     return (
         <>
         <div className='ui search'>
@@ -16,12 +19,58 @@ const Search = ({ placeholder, handleChange }) => {
                     placeholder={placeholder}
                     onChange={handleChange}
                 />
-                <button></button>
+                <button onClick={() => 
+                fetchMovies(dispatch, searchMovie)}>test</button>
             </div>
         </div>
         </>
     );
 }
+ 
+async function fetchMovies(dispatch, searchWord) {
+    dispatch(actions.isFetching());
+    console.log('Got search word: ', searchWord);
+    const url = 'http://www.omdbapi.com/?apikey=72d7fe9&s=' + searchWord
+    try {
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log('Got data: ', json);
+        const pages = (Math.floor(parseInt(json.totalResults) / 10)) + 1
+        let movies = json.Search;
+        dispatch(actions.clearMovies())
+        movies.map(movie=>{
+            dispatch(actions.success(movie))
+        })
+  //      dispatch(actionssetCurrentScreen.setCurrentScreen('movie'))
+        console.log('the moviessss : ', movies)
+        for (let index = 2; index < pages + 1; index++){
+            console.log('for loop', index)
+            fetchRestOfMovies(dispatch, searchWord, index)
+           
+        }
+    } catch {
+        dispatch(actions.failure());
+
+    }
+}
+
+async function fetchRestOfMovies(dispatch, searchWord, page){
+    console.log('fetching page', page)
+    const url = 'http://omdbapi.com/?apikey=72d7fe9&s=' + searchWord + '&page=' + page
+    try {
+        let response = await fetch(url)
+        let json = await response.json()
+        let movies = json.Search;
+        movies.map(movie=>{
+            dispatch(actions.success(movie))
+        })
+  
+  //      console.log('the moviessss : ', movies)
+    } catch {
+        dispatch(actions.failure());
+
+    }
     
+}
 
 export default Search;
