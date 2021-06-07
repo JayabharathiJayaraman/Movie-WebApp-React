9d8d670e18bcf719aca7e2b47650d0417093478e
Sun-S2 <underskin1@gmail.com>
2021-06-07 15:10:10 +0200

    Movie layout

--- src/components/HighlightedMovie.jsx
+++ src/components/HighlightedMovie.jsx
@@ -20,43 +20,52 @@ const HighlightedMovie = () => {
         if (elm) {
             elm.addEventListener('click', () => {
                 document.querySelector('#overlay').classList.remove('show');
-
+                document.body.style.overflow = 'unset';
             });
         }
     }
 
     useEffect(() => {
-        
         document.querySelector('#overlay').classList.add('show');
+        document.body.style.overflow = 'hidden';
         console.log(selectedmovie.Title)
     }, [selectedmovie]);
    
    
     return (
         
-            <section id="overlay">
-                <figure>
-                    <button id='closebutton' onClick={()=>{
-                        closelightbox();
-                    }}>X</button>
-                    
-                    
-                    <img src={selectedmovie.Poster} alt={selectedmovie.Title} />
-                    <figcaption>
-                    Title:  {selectedmovie.Title}<br/>
-                    Year: {selectedmovie.Year}<br/>
-                    Time: {selectedmovie.Runtime}<br/>
-                    Language: {selectedmovie.Language}<br/>
-                    Ratings: 
-                    <StarsRating  count={5}  onChange={ratingChanged}  size={20}  value={selectedmovie.imdbRating/2}  edit={false}  color2={'#51E706'} color1={'#F8F3F1'} />
-                    </figcaption>
-                    
-                     <div id='movierating'>
-                         {/*<StarsRating  count={5}  onChange={ratingChanged}  size={24}  value={4.7}  edit={false}  color2={'#51E706'} color1={'#F8F3F1'} />*/}
-                     </div>
-                </figure>
-            </section>
-        
+             <section id="overlay">
+                <div className="figure">
+                    <div className="topContainer">
+                        <div className="imgDisp">
+                            <img src={selectedmovie.Poster} alt={selectedmovie.Title}/>
+                        </div>
+                        <div className="sideContainer">
+                            <div className="titlesDisp">
+                                <div className="figcaption">
+                                <h1 className='styleH1txt'>{selectedmovie.Title}</h1><br/>
+                                <h3 className='styleH3txt'>{selectedmovie.Year}</h3>
+                                <h3 className='styleH3txt'>&nbsp;|&nbsp;{selectedmovie.Runtime}</h3>
+                                <h3 className='styleH3txt'>&nbsp;|&nbsp;{selectedmovie.Language}</h3><br/>
+                                Ratings:
+                                <StarsRating  count={5}  onChange={ratingChanged}  size={20}
+                                    value={selectedmovie.imdbRating/2}  edit={false}  color2={'#51E706'} color1={'#F8F3F1'}/>
+
+
+
+{/*                                     <div className="rateInner"><DisplayingStars/></div> */}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottomContainer clear">
+                        <div className="bottomContTitle"><h2>Comments</h2></div>
+{/*                         <div className="commentsBody"><DisplayThisList/></div> */}
+                        <div className="clear"></div>
+                    </div>
+               </div>
+             </section>
     )
 
 
