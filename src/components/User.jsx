21c0dca7c0b4f281eeae372ba31f24729d4b860f
zakariamosa <zakariamosa1211@gmail.com>
2021-06-07 13:51:54 +0200

    working with rating

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -8,6 +8,7 @@ import MovieCard from './MovieCard';
 import { actionsLogin } from "../features/login";
 
 import HighlightedMovie from './HighlightedMovie';
+import db from '../features/firebase';
 
 
 <link rel="stylesheet" href="node_modules/react-star-rating/dist/css/react-star-rating.min.css"></link>
@@ -17,10 +18,13 @@ const User = () => {
 
     
     const [content, setContent] = useState(null)
+    const [movierating, setMovierating] = useState(5)
+    const [moviecomment, setMoviecomment] = useState('abc')
     const currentloginuser = useSelector(state => state.login.currentuser);
     const currentusershppinghistory = useSelector(state => state.shoppinghistory.currentusershoppinghistory);
     const ratingChanged = (newRating) => {
         console.log(newRating)
+        setMovierating(newRating)
       }
     useEffect(() => {
 
@@ -32,15 +36,39 @@ const User = () => {
                 <StarsRating  count={5}  onChange={ratingChanged}  size={36}  value={5}  edit={true}  color2={'#51E706'} color1={'#F8F3F1'} />
                 comment:
                 <input    type="text" />
-                <button onClick={shoot}>Send Rating!</button>
+                <button onClick={()=>{
+                shoot(checkout.movie.imdbID)
+                }} onChange={leaveacomment}>Send Rating!</button>
             </div>
         ))
     }, []);
 
+    function leaveacomment(val) {
+        console.warn(val.target.value);
+        setMoviecomment(val.target.value);
+        
+    }
 
-    function shoot() {
+
+    function shoot(imdbID) {
+        saveuserratingtodb(imdbID)
         alert("Thank you for your rating !!");
       }
+      const saveuserratingtodb=async(imdbID)=>{
+        db.collection("ratings").doc().set({
+            imdbID: imdbID,
+            movierating: movierating,
+            usercomment: moviecomment,
+            uid: currentloginuser.uid
+        })
+        .then(function() {
+            
+            
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+    }
    
 
     
