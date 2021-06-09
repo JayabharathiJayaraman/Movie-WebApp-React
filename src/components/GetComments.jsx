3cf398b466d3a3ca7d46cc8cf147af0387db500b
David Dahlman <daviddahlman82@gmail.com>
2021-06-09 13:23:26 +0200

    fix comments

--- src/components/GetComments.jsx
+++ src/components/GetComments.jsx
@@ -1,20 +1,25 @@
 import db from '../features/firebase';
+import StarsRating from 'stars-rating';
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-const GetComments = (props) =>  {
+const GetComments = () =>  {
 
+
+    const selectedmovie = useSelector(state => state.highlightmovie.selectedmovie);
     const [comments, setComments] = useState([])
     const [loading, setLoading] = useState(false)
     
     const ref = db.collection("ratings")
     
-    function getComments(props) {
+    function getComments() {
         setLoading(true)
         ref.onSnapshot((querySnapshot) => {
             const items = []
             querySnapshot.forEach((doc) => {
-                if(props.imdb == doc.data().imdbID){
+                if(selectedmovie.imdbID == doc.data().imdbID){
                     items.push(doc.data())
+                    console.log('comment data', doc.data())
                 }
             })
             setComments(items)
@@ -23,17 +28,24 @@ const GetComments = (props) =>  {
     }
     
     useEffect(()=>{
-        getComments(props)
-    }, [])
+        getComments()
+    }, [selectedmovie])
 
     if (loading) {
-        return <h1>Loading...</h1>
+        return <h2>Loading...</h2>
     }
     return (
         <div>
         {comments.map((comment)=>(
-            <div>{comment.usercomment}</div>
+            <div>
+            <div className='levelRating'><StarsRating  count={5}   size={25} 
+                value= {4} edit={false} 
+                color2={'#ffcc33'} color1={'#F8F3F1'}/></div>
+            <h3 >{comment.usercomment}</h3>
+            {comment.movierating}
+            </div>
         ))}
+        <h4>{comments.length}</h4>
         </div>
     )
 
